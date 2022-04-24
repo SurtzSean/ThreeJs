b0c18cce10fd9d2c0144faf0bb2ec150e50b9516
Sean Surtz <seansurtz@Seans-Mac-mini.local>
2022-04-23 20:16:02 -0400

    first render

---
+++ 1.1 BasicScene/scene/script.js
@@ -0,0 +1,30 @@
+const scene = new THREE.Scene()
+
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+camera.position.z = 5
+scene.add(mesh)
+scene.add(camera)
+
+//Renderer
+const canvas = document.querySelector('.webgl')
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+})
+
+renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.render(scene, camera)
+
+//rotate cube
+const animate = () => {
+    requestAnimationFrame(animate)
+    mesh.rotation.x += 0.01
+    mesh.rotation.y += 0.01
+    renderer.render(scene, camera)
+}
+
+//call animate
+animate()
